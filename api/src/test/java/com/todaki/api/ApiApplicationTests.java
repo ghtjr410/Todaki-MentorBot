7a4074eee010303061f92b7ded090641feb5bd25
ghtjr410 <ghtjr410@gmail.com>
2025-03-13 18:02:19 +0900

    프로젝트 초기 설정 - 의존성   - spring web, spirng jpa, postgresql, lombok

---
+++ api/src/test/java/com/todaki/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todaki.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
